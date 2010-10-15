44a5419ee762aae4738c465dfdac96c06a1452cb
tomi <tomi@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-10-15 02:59:45 -0500

    copy from code.google.com
    git-svn-id: http://svn.symfony-project.com/plugins/sfThriftPlugin/trunk@31143 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ config/sfThriftPluginConfiguration.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * sfThriftPlugin configuration.
+ *
+ * @package     sfThriftPlugin
+ * @subpackage  config
+ * @author      Arkadiusz Tułodziecki
+ * @version     SVN: $Id: sfThriftPluginConfiguration.class.php 6 2010-06-17 11:22:49Z atulodzi $
+ */
+class sfThriftPluginConfiguration extends sfPluginConfiguration {
+	const VERSION = '1.0.0-DEV';
+	
+	/**
+	 * @see sfPluginConfiguration
+	 */
+	public function initialize() {
+	}
+}
