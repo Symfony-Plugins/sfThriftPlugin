98ab2907dceeedab965d2b98437885e571ff9c0a
tomi <tomi@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-11-01 11:34:08 -0500

    new config params
    git-svn-id: http://svn.symfony-project.com/plugins/sfThriftPlugin/trunk@31304 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/factory/ThriftProtocolFactory.class.php
+++ lib/factory/ThriftProtocolFactory.class.php
@@ -13,35 +13,37 @@ final class ThriftProtocolFactory {
 	public static final function factory($configPostfix = 'default') {
 		$config = sfConfig::get('app_thrift_plugin_'.$configPostfix);
 		if(
-			empty($config['transport'][0]) ||
-			empty($config['transport'][1]) ||
+			empty($config['connector']) ||
+			empty($config['transport']) ||
 			empty($config['protocol'])
 		) {
 			throw new Exception('Bad Thrift config');
 		}
 
-		$transport0 = self::getTransport0($config);
-		$transport0->open();
-		$transport1 = self::getTransport1($config, $transport0);
+		$connector = self::getConnector($config['connector']);
+		$connector->open();
+		$transport = self::getTransport($config['transport'], $connector);
 
-		return self::getProtocol($config, $transport1);
+		return self::getProtocol($config['protocol'], $transport);
 	}
 
 	/**
 	 *
-	 * @param array $config
+	 * @param array $params
 	 * @return TTransport
 	 */
-	private static function getTransport0($config) {
-		switch($config['transport'][0]) {
+	private static function getConnector($config) {
+		$class = isset($config['class']) ? $config['class'] : '';
+		$params = isset($config['params']) ? $config['params'] : array();
+		switch($class) {
 			case 'THttpClient':
-				if(!isset($config['host'])) throw new Exception ('Bad Thrift transport config');
+				if(!isset($params['host'])) throw new Exception ('Bad Thrift transport config');
 
-				$host = $config['host'];
-				$port = isset($config['port']) ? $config['port'] : 80;
-				$uri = isset($config['uri']) ? $config['uri'] : '';
-				$scheme = isset($config['scheme']) ? $config['scheme'] : 'http';
-				$timeout = isset($config['timeout']) ? $config['timeout'] : null;
+				$host = $params['host'];
+				$port = isset($params['port']) ? $params['port'] : 80;
+				$uri = isset($params['uri']) ? $params['uri'] : '';
+				$scheme = isset($params['scheme']) ? $params['scheme'] : 'http';
+				$timeout = isset($params['timeout']) ? $params['timeout'] : null;
 
 				$transport = new THttpClient($url, $port, $uri, $scheme);
 				$transport->setTimeoutSecs($timeout);
@@ -49,35 +51,35 @@ final class ThriftProtocolFactory {
 				sfContext::getInstance()->getLogger()->info(sprintf('{sfThriftPlugin}Create THttpClient with parameters: host = "%s", port = %d, uri = "%s", scheme = "%s", timeout = %d', $host, $port, $uri, $scheme, $timeout));
 				break;
 			case 'TMemoryBuffer':
-				$buf = isset($config['buf']) ? $config['buf'] : '';
+				$buf = isset($params['buf']) ? $params['buf'] : '';
 
 				$transport = new TMemoryBuffer($buf);
 
 				sfContext::getInstance()->getLogger()->info(sprintf('{sfThriftPlugin}Create TMemoryBuffer with parameters: buf = "%s"', $buf));
 				break;
 			case 'TPhpStream':
-				if(!isset($config['mode'])) throw new Exception ('Bad Thrift transport config');
+				if(!isset($params['mode'])) throw new Exception ('Bad Thrift transport config');
 
-				$mode = $config['mode'];
+				$mode = $params['mode'];
 
 				$transport = new TPhpStream($mode);
 
 				sfContext::getInstance()->getLogger()->info(sprintf('{sfThriftPlugin}Create TPhpStream with parameters: mode = %d', $mode));
 				break;
 			case 'TServerSocket':
-				$host = isset($config['host']) ? $config['host'] : 'localhost';
-				$port = isset($config['port']) ? $config['port'] : 9090;
+				$host = isset($params['host']) ? $params['host'] : 'localhost';
+				$port = isset($params['port']) ? $params['port'] : 9090;
 
 				$transport = new TServerSocket($host, $port);
 
 				sfContext::getInstance()->getLogger()->info(sprintf('{sfThriftPlugin}Create TServerSocket with parameters: host = "%s"', $host, $port));
 				break;
 			case 'TSocket':
-				$host = isset($config['host']) ? $config['host'] : 'localhost';
-				$port = isset($config['port']) ? $config['port'] : 9090;
-				$persist = isset($config['persist']) ? $config['persist'] : false;
-				$send_timeout = isset($config['send_timeout']) ? $config['send_timeout'] : 100;
-				$recv_timeout = isset($config['recv_timeout']) ? $config['recv_timeout'] : 750;
+				$host = isset($params['host']) ? $params['host'] : 'localhost';
+				$port = isset($params['port']) ? $params['port'] : 9090;
+				$persist = isset($params['persist']) ? $params['persist'] : false;
+				$send_timeout = isset($params['send_timeout']) ? $params['send_timeout'] : 100;
+				$recv_timeout = isset($params['recv_timeout']) ? $params['recv_timeout'] : 750;
 
 				$transport = new TSocket($host, $port, $persist);
 				$transport->setSendTimeout($send_timeout);
@@ -86,11 +88,11 @@ final class ThriftProtocolFactory {
 				sfContext::getInstance()->getLogger()->info(sprintf('{sfThriftPlugin}Create TSocket with parameters: host = "%s", port = %d, persist = %s, send_timeout = %d, recv_timeout = %d', $host, $port, $persist ? 'true' : 'false', $send_timeout, $recv_timeout));
 				break;
 			case 'TSocketPool':
-				$hosts = isset($config['hosts']) ? $config['hosts'] : array('localhost');
-				$ports = isset($config['ports']) ? $config['ports'] : array(9090);
-				$persist = isset($config['persist']) ? $config['persist'] : false;
-				$send_timeout = isset($config['send_timeout']) ? $config['send_timeout'] : 100;
-				$recv_timeout = isset($config['recv_timeout']) ? $config['recv_timeout'] : 750;
+				$hosts = isset($params['hosts']) ? $params['hosts'] : array('localhost');
+				$ports = isset($params['ports']) ? $params['ports'] : array(9090);
+				$persist = isset($params['persist']) ? $params['persist'] : false;
+				$send_timeout = isset($params['send_timeout']) ? $params['send_timeout'] : 100;
+				$recv_timeout = isset($params['recv_timeout']) ? $params['recv_timeout'] : 750;
 
 				$transport = new TSocket($host, $port, $persist);
 				$transport->setSendTimeout($send_timeout);
@@ -99,7 +101,7 @@ final class ThriftProtocolFactory {
 				sfContext::getInstance()->getLogger()->info(sprintf('{sfThriftPlugin}Create TSocketPool with parameters: hosts = ("%s"), ports = (%d), persist = %s, send_timeout = %d, recv_timeout = %d', implode('","', $hosts), implode('","', $ports), $persist ? 'true' : 'false', $send_timeout, $recv_timeout));
 				break;
 			default:
-				throw new Exception('Unknown transport: '.$config['transport'][0]);
+				throw new Exception('Unknown transport: '.$class);
 		}
 
 		return $transport;
@@ -107,64 +109,70 @@ final class ThriftProtocolFactory {
 
 	/**
 	 *
-	 * @param array $config
-	 * @param TTransport $transport
+	 * @param array $params
+	 * @param TTransport $connector
 	 * @return TTransport
 	 */
-	private static function getTransport1($config, TTransport $transport) {
-		switch($config['transport'][1]) {
+	private static function getTransport($config, TTransport $connector) {
+		$class = isset($config['class']) ? $config['class'] : '';
+		$params = isset($config['params']) ? $config['params'] : array();
+		switch($class) {
 			case 'TBufferedTransport':
-				$rBufSize = isset($config['read_buf_size']) ? $config['read_buf_size'] : 512;
-				$wBufSize = isset($config['write_buf_size']) ? $config['write_buf_size'] : 512;
+				$rBufSize = isset($params['read_buf_size']) ? $params['read_buf_size'] : 512;
+				$wBufSize = isset($params['write_buf_size']) ? $params['write_buf_size'] : 512;
 
-				$transport = new TBufferedTransport($transport, $rBufSize, $wBufSize);
+				$connector = new TBufferedTransport($connector, $rBufSize, $wBufSize);
 
 				sfContext::getInstance()->getLogger()->info(sprintf('{sfThriftPlugin}Create TBufferedTransport with parameters: read_buf_size = %d, write_buf_size = %d', $rBufSize, $wBufSize));
 				break;
 			case 'TFramedTransport':
-				$read = isset($config['read']) ? $config['read'] : true;
-				$write = isset($config['write']) ? $config['write'] : true;
+				$read = isset($params['read']) ? $params['read'] : true;
+				$write = isset($params['write']) ? $params['write'] : true;
 
-				$transport = new TFramedTransport($transport, $read, $write);
+				$connector = new TFramedTransport($connector, $read, $write);
 
 				sfContext::getInstance()->getLogger()->info(sprintf('{sfThriftPlugin}Create TFramedTransport with parameters: read = %s, write = %s', $read ? 'true' : 'false', $write ? 'true' : 'false'));
 				break;
 			case 'TNullTransport':
-				$transport = new TNullTransport();
+				$connector = new TNullTransport();
 
 				sfContext::getInstance()->getLogger()->info(sprintf('{sfThriftPlugin}Create TNullTransport'));
 				break;
 			default:
-				throw new Exception('Unknown transport: '.$config['transport'][1]);
+				throw new Exception('Unknown transport: '.$class);
 		}
 
-		return $transport;
+		return $connector;
 	}
 
 	/**
 	 *
-	 * @param array $config
+	 * @param array $params
 	 * @param TTransport
 	 * @return TProtocol
 	 */
 	private static function getProtocol($config, TTransport $transport) {
-		switch($config['protocol']) {
+		$class = isset($config['class']) ? $config['class'] : '';
+		$params = isset($config['params']) ? $config['params'] : array();
+		switch($class) {
 			case 'TBinaryProtocol':
-				$strictRead = isset($config['strict_read']) ? $config['strict_read'] : false;
-				$strictWrite = isset($config['strict_write']) ? $config['strict_write'] : true;
+				$strictRead = isset($params['strict_read']) ? $params['strict_read'] : false;
+				$strictWrite = isset($params['strict_write']) ? $params['strict_write'] : true;
 
 				$protocol = new TBinaryProtocol($transport, $strictRead, $strictWrite);
 
 				sfContext::getInstance()->getLogger()->info(sprintf('{sfThriftPlugin}Create TBinaryProtocol with parameters: strictRead = %s, strictWrite = %s', $strictRead ? 'true' : 'false', $strictWrite ? 'true' : 'false'));
 				break;
 			case 'TBinaryProtocolAccelerated':
-				$strictRead = isset($config['strict_read']) ? $config['strict_read'] : false;
-				$strictWrite = isset($config['strict_write']) ? $config['strict_write'] : true;
+				$strictRead = isset($params['strict_read']) ? $params['strict_read'] : false;
+				$strictWrite = isset($params['strict_write']) ? $params['strict_write'] : true;
 
 				$protocol = new TBinaryProtocolAccelerated($transport, $strictRead, $strictWrite);
 
 				sfContext::getInstance()->getLogger()->info(sprintf('{sfThriftPlugin}Create TBinaryProtocolAccelerated with parameters: strictRead = %s, strictWrite = %s', $strictRead ? 'true' : 'false', $strictWrite ? 'true' : 'false'));
 				break;
+			default:
+				throw new Exception('Unknown protocol: '.$class);
 		}
 
 		return $protocol;
